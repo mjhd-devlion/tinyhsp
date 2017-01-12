915e4c444c66ad7c3b030f8c92d91006234e0024
dolphilia <satoshi.hanzawa@gmail.com>
2017-01-12 17:55:08 +0900

    ファイル読み込むユーティリティを追加

---
+++ src/utility_file.h
@@ -0,0 +1,4 @@
+#pragma once
+
+char** malloc_file_to_buffer(const char* file_name, char** line_buffer, int* line_length, int* line_number); // ファイルから行ごとに二次元配列に格納して返す
+void free_line_buffer(char** line_buffer, int line_num); // 確保した二次元配列を解放する
