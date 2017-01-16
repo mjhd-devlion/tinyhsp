55ac01da20b72f6368ef46f1426b9d8c64f2ff9e
dolphilia <satoshi.hanzawa@gmail.com>
2017-01-16 17:16:56 +0900

    テスト用のコードを追加

--- src/parser.c
+++ src/parser.c
@@ -5,8 +5,72 @@
 #include "lexer.h"
 #include "parser.h"
 
+static void my_get_token(Token* token);
+static void unget_token(Token* token);
+static double parser_primary_expression();
+static double parser_term();
+
 static Token st_token;
 static int st_token_exists;
+
+// テスト用コード
+/*
+int
+main(int argc, char** argv)
+{
+	char line[LINE_BUF_SIZE];
+	double value;
+	while (fgets(line, LINE_BUF_SIZE, stdin) != NULL) {
+		set_line(line);
+		value = parse_line();
+		printf(">>%f\n", value);
+	}
+	return 0;
+}
+
+double
+parse_line(void)
+{
+	double value;
+	st_token_exists = 0;
+	value = parse_expression();
+	return value;
+}
+*/
+//
+
+void set_st_token_exists(int value) {
+	st_token_exists = value;
+}
+
+double
+parser_expression() {
+	double v1;
+	double v2;
+	Token token;
+	
+	v1 = parser_term();
+	for (;;) {
+		my_get_token(&token);
+		if (token.group != ADD_TOKEN
+			&& token.group != SUB_TOKEN) {
+			unget_token(&token);
+			break;
+		}
+		v2 = parser_term();
+		if (token.group == ADD_TOKEN) {
+			v1 = v1 + v2;
+		} else if (token.group == SUB_TOKEN) {
+			v1 = v1 - v2;
+		} else {
+			unget_token(&token);
+		}
+	}
+	
+	return v1;
+}
+
+//
 
 static void
 my_get_token(Token* token) {
@@ -80,35 +144,4 @@ parser_term() {
 	}
 	
 	return v1;
-}
-
-double
-parser_expression() {
-	double v1;
-	double v2;
-	Token token;
-	
-	v1 = parser_term();
-	for (;;) {
-		my_get_token(&token);
-		if (token.group != ADD_TOKEN
-			&& token.group != SUB_TOKEN) {
-			unget_token(&token);
-			break;
-		}
-		v2 = parser_term();
-		if (token.group == ADD_TOKEN) {
-			v1 = v1 + v2;
-		} else if (token.group == SUB_TOKEN) {
-			v1 = v1 - v2;
-		} else {
-			unget_token(&token);
-		}
-	}
-	
-	return v1;
-}
-
-void set_st_token_exists(int value) {
-	st_token_exists = value;
 }
